b59968c875ab1796734faafe15f112f0b63ad6bd
Shih Feng Lin <noahleft@gmail.com>
2013-12-20 16:54:18 +0800

    Initial Commit

---
+++ STA/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  STA
+//
+//  Created by lin shih-feng on 2013/12/20.
+//  Copyright (c) 2013年 lin shih-feng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
